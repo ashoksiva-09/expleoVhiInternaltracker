0f98e2c91e6974bb41b56c5560a1baec05830de1
Ashokkumar Sivakumar <Ashokkumar.Sivakumar@expleogroup.com>
2025-09-08 19:31:19 +0530

    Your commit message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;  
+const PORT = 5001;  
 
 const initDb = () => {
     return new Promise((resolve, reject) => {
@@ -876,8 +876,8 @@ app.post('/api/bold-minds', (req, res) => {
 
 // Start the server
 initDb().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Dashboard running at http://localhost:${PORT}/index.html`);
+    app.listen(PORT, '10.192.190.158', () => {
+        console.log(`Dashboard running at http://10.192.190.158/:${PORT}/index.html`);
     });
 }).catch((err) => {
     console.error('Failed to initialize database:', err);
